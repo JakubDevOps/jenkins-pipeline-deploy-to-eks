ad2ca8b6c8588c37d973b9e4460d44fd4d7f2931
JakubDevOps <jakub.grzadzielski@gmail.com>
2023-08-28 12:27:41 +0200

    8

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "azs" {}
 module "myapp-vpc" {
-  source          = "terraform-aws-modules/terraform-aws-vpc"
-  version         = "5.0.0"
+  source          = "terraform-aws-modules/vpc/aws"
+  # version         = "5.0.0"
   name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
